de20ec3a546c725638b668568fd425de6df7f403
ybaha <ybahaerarslan@gmail.com>
2020-05-02 02:11:33 +0300

    Initial commit

---
+++ client.js
@@ -0,0 +1,253 @@
+
+const input = document.getElementById("input");
+const form = document.getElementById("form");
+const messagesBox = document.getElementById("messages-box");
+
+let messages = [];
+
+var socket = io("localhost:3000");
+
+var name = prompt("enter your name");
+
+socket.emit("connection", name)
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevents page reloading
+  const msg = input.value;
+  socket.emit("chat-message", { message: msg, username: name })
+  input.value = ""
+});
+
+socket.on("connection", (msgs)=>{
+  console.log("msjlar geldi");
+  console.log(msgs);
+  
+  
+  messages = msgs;
+  displayMessages(msgs);
+})
+
+socket.on("chat-message", (msgs) => {
+  displayMessages(msgs);
+});
+
+
+
+//--------------------------------------
+
+var players = [];
+var deck = [];
+var me;
+var currentPlayer;
+
+function refreshPlayersCards() {
+  let players_div = document.getElementsByClassName("players");
+  if (players_div.childNodes != undefined) {
+    for (let i = 0; i < players_div.length; i++) {
+      players_div.childNodes[i].remove();
+    }
+  }
+}
+
+socket.on("start-game", (data) => {
+  let playersD = document.getElementById("players");
+  refreshPlayersCards();
+  // currentplayer deck players datası
+  playersD.style.display = "flex";
+  players = data.players;
+  deck = data.deck;
+  currentPlayer = data.currentPlayer;
+
+  document.getElementById("status").style.display = "none";
+  // deal 2 cards to every player object
+  createPlayersUI();
+  dealHands();
+  isMyTurn();
+  document.getElementById('player_' + data.currentPlayer).classList.add('active');
+})
+function createPlayersUI() {
+  document.getElementById('players').innerHTML = '';
+  for (var i = 0; i < players.length; i++) {
+    var div_player = document.createElement('div');
+    var div_playerid = document.createElement('div');
+    var div_hand = document.createElement('div');
+    var div_points = document.createElement('div');
+
+    div_points.className = 'points';
+    div_points.id = 'points_' + i;
+    div_player.id = 'player_' + i;
+    div_player.className = 'player';
+    div_hand.id = 'hand_' + i;
+
+    div_playerid.innerHTML = players[i].name;
+    div_player.appendChild(div_playerid);
+    div_player.appendChild(div_hand);
+    div_player.appendChild(div_points);
+
+    document.getElementById('players').appendChild(div_player);
+  }
+}
+
+function isMyTurn() {
+
+  me = players.filter((e) => {
+    if (name == e.name) {
+      return e;
+    }
+  })
+  var buttons = document.getElementsByClassName("btn");
+
+  if (me[0].num != currentPlayer) {
+    for (let i = 0; i < buttons.length; i++)
+      buttons[i].style.display = "none"
+  }
+  else {
+    for (let i = 0; i < buttons.length; i++)
+      buttons[i].style.display = "inline-block"
+  }
+}
+
+function dealHands() {
+  // alternate handing cards to each player
+  // 2 cards each
+  
+  for (var i = 0; i < 2; i++) {
+    for (var x = 0; x < players.length; x++) {
+      renderCard(players[x].Hand[i], x);
+      updatePoints();
+    }
+  }
+
+}
+
+function renderCard(card, player) {
+  var hand = document.getElementById('hand_' + player);
+  hand.appendChild(getCardUI(card));
+}
+
+function getCardUI(card) {
+  var el = document.createElement('div');
+  var icon = '';
+  if (card.Suit == 'Hearts')
+    icon = '&hearts;';
+  else if (card.Suit == 'Spades')
+    icon = '&spades;';
+  else if (card.Suit == 'Diamonds')
+    icon = '&diams;';
+  else
+    icon = '&clubs;';
+
+  el.className = 'card';
+  el.innerHTML = card.Value + '<br/>' + icon;
+  return el;
+}
+
+// returns the number of points that a player has in hand
+function getPoints(player) {
+  var points = 0;
+  for (var i = 0; i < players[player].Hand.length; i++) {
+    points += players[player].Hand[i].Weight;
+  }
+  players[player].Points = points;
+  return points;
+}
+
+function updatePoints() {
+  for (let i = 0; i < players.length; i++) {
+    getPoints(i);
+    document.getElementById('points_' + i).innerHTML = players[i].Points;
+  }
+}
+
+function hit() {
+  socket.emit("hit", me)
+
+}
+
+function stay() {
+  // oyuncu sonuncu kisi degilse
+  if (currentPlayer < players.length - 1) {
+    socket.emit("stay", currentPlayer)
+    currentPlayer++;
+    isMyTurn();
+  }
+  //oyuncu sonuncu kisi ise
+  else {
+    let buttons = document.getElementsByClassName("btn");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.display = "none";
+    }
+    socket.emit("game-over")
+  }
+}
+
+socket.on("cur-update", (cP) => {
+  currentPlayer = cP;
+  var playersD = document.getElementById("players");
+  for(let i = 0; i < playersD.childNodes.length; i++){
+    playersD.childNodes[i].className = "player"
+  }
+  playersD.childNodes[cP].className = "player active"
+  isMyTurn();
+})
+
+socket.on("end", (winner) => {
+  console.log("game ended" + winner);
+  end(winner);
+});
+
+socket.on("update-cards", (data) => {
+  console.log(data);
+  console.log(data.me[0].num);
+  renderCard(data.card, data.me[0].num);
+  let card = deck.pop();
+  players[data.me[0].num].Hand.push(card);
+  updatePoints();
+  if (me[0].Points > 21) {
+    stay();
+  }
+  console.log("update-cards geldi calisti");
+  console.log(players);
+
+})
+
+socket.on("disconnect-update", (playersData) => {
+  console.log("remaining");
+  console.log(playersData);
+  players = playersData;
+  if (players.length > 1) {
+    createPlayersUI();
+    dealHands();
+    isMyTurn();
+  }
+  else {
+    console.log(players[0]);
+    
+    end(players[0].num);
+  }
+})
+
+function end(winner) {
+  var status = document.getElementById('status');
+  var playersD = document.getElementById('players');
+
+  status.innerHTML = 'Winner: ' + players[winner].name;
+  status.style.display = "inline-block";
+  playersD.style.display = "none";
+}
+
+function displayMessages(msgs){
+  let lis = document.querySelectorAll("#messages-box li");
+  if (messagesBox.hasChildNodes) {
+    for(var i=0; li=lis[i]; i++) {
+      li.parentNode.removeChild(li);
+    }
+  }
+
+  for (let i = 0; i < msgs.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = msgs[i].username + ": " + msgs[i].message;
+    messagesBox.appendChild(li);
+  }
+}
+
---
+++ index.js
@@ -0,0 +1,229 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/');
+});
+
+let messages = []
+
+var deck = [];
+var players = [];
+var playersInQueue = [];
+var currentPlayer = 0;
+var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+var isGameRunning;
+
+function playerNums() {
+  if (players.length > 0) {
+    for (let i = 0; i < players.length; i++) {
+      players[i].num = i;
+    }
+  }
+}
+
+///////----------------------
+
+
+//checks if game stuck
+setInterval(() => {
+  if(isGameRunning == true && players.length < 1){
+    endGame();
+  }
+},2000)
+
+
+function createDeck() {
+  deck = new Array();
+  for (var i = 0; i < values.length; i++) {
+    for (var x = 0; x < suits.length; x++) {
+      var weight = parseInt(values[i]);
+      if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+        weight = 10;
+      if (values[i] == "A")
+        weight = 11;
+      var card = { Value: values[i], Suit: suits[x], Weight: weight };
+      deck.push(card);
+    }
+  }
+}
+
+function shuffle() {
+  // for 1000 turns
+  // switch the values of two random cards
+  for (var i = 0; i < 1000; i++) {
+    var l1 = Math.floor((Math.random() * deck.length));
+    var l2 = Math.floor((Math.random() * deck.length));
+    var tmp = deck[l1];
+    deck[l1] = deck[l2];
+    deck[l2] = tmp;
+  }
+}
+
+function startblackjack() {
+  // deal 2 cards to every player object
+  isGameRunning = true;
+  currentPlayer = 0;
+  players = players.concat(playersInQueue);
+
+  for (let i = 0; i < players.length; i++) {
+    players[i].Points = 0;
+    players[i].Hand = [];
+  }
+
+  createDeck();
+  shuffle();
+  dealHands();
+
+  io.emit("start-game", { deck, players, currentPlayer, players })
+}
+
+function waitingForPlayers() {
+  var waitingForPlayersInterval = setInterval(() => {
+    if (players.length > 1) {
+      console.log("oyun basladi");
+      startblackjack();
+      clearInterval(waitingForPlayersInterval);
+    }
+  }, 5000)
+}
+
+waitingForPlayers();
+
+function dealHands() {
+  // alternate handing cards to each player
+  // 2 cards each
+  for (var i = 0; i < 2; i++) {
+    for (var x = 0; x < players.length; x++) {
+      var card = deck.pop();
+      players[x].Hand.push(card);
+      updatePoints();
+    }
+  }
+}
+
+// returns the number of points that a player has in hand
+function getPoints(player) {
+  var points = 0;
+  for (var i = 0; i < players[player].Hand.length; i++) {
+    points += players[player].Hand[i].Weight;
+  }
+  players[player].Points = points;
+  return points;
+}
+
+function updatePoints() {
+  for (var i = 0; i < players.length; i++) {
+    getPoints(i);
+  }
+}
+io.on("connection", (socket) => {
+  socket.on("hit", (me) => {
+    console.log(me.name + " hitted");
+    var card = deck.pop();
+    players[currentPlayer].Hand.push(card);
+    updatePoints();
+    io.emit("update-cards", {card, me, messages})
+    if (me[0].Points > 21) {
+      stay();
+    }
+  })
+  socket.on("stay", (cP) => {
+    stay();
+  })
+  socket.on("game-over", () => {
+    io.emit("end", endGame())
+  })
+  socket.on("connection", (username) => {
+    let hand = [];
+    if (isGameRunning == true) {
+      let player = { name: username, id: socket.id, Points: 0, Hand: hand, num: 0 };
+      playersInQueue.push(player);
+    }
+    else {
+      let player = { name: username, id: socket.id, Points: 0, Hand: hand, num: 0 };
+      players.push(player);
+      playerNums();
+    }
+  })
+  socket.on('chat-message', (data) => {
+    if(messages.length>12){
+      messages.shift();
+    }
+    messages.push(data);
+    io.emit("chat-message", messages);
+  });
+  socket.on("disconnect", () => {
+    players = players.filter(players => players.id != socket.id)
+    playerNums();
+
+    io.emit("disconnect-update", players);
+
+    if(players.length<2){
+      io.emit("end", endGame())
+    }
+  })
+})
+
+function stay() {
+  if (currentPlayer < players.length - 1) {
+    currentPlayer++;
+    io.emit("cur-update", currentPlayer);
+  }
+  else {
+  }
+
+}
+
+function endGame() {
+
+  for (let i = 0; i < players.length; i++) {
+    console.log(players[i].name + ": " + players[i].Points)
+  }
+
+  var winner = -1;
+  var score = 0;
+
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].Points > score && players[i].Points < 22) {
+      winner = i;
+    }
+    score = players[i].Points;
+  }
+  isGameRunning = false;
+  if((players.length+playersInQueue.length) > 1){
+    startNewGame();
+  }
+  else{
+    waitingForPlayers();
+  }
+  return winner;
+}
+
+function startNewGame() {
+  let i = 6;
+  let interval = setInterval(() => {
+    if (i < 0) {
+      isGameRunning = true;
+      startblackjack();
+      clearInterval(interval)
+    }
+    else
+      i--;
+  }, 1000)
+}
+
+
+function check() {
+  if (players[currentPlayer].Points > 21) {
+    end();
+  }
+}
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
