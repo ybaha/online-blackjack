4ce59232715ff5b2140b4de73f5773a184a7f410
ybaha <ybahaerarslan@gmail.com>
2020-05-08 09:25:19 +0300

    heroku

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/');
 });
+
+http.listen(PORT, () => {
+  console.log('listening on *:3000');
+});
+
 
 let messages = []
 
@@ -224,6 +231,3 @@ function check() {
 }
 
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
