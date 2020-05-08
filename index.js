b6c718fa994692f572b32a0be05ab0a1ff29fcf1
ybaha <ybahaerarslan@gmail.com>
2020-05-08 10:09:15 +0300

    add public file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(PORT, () => {
